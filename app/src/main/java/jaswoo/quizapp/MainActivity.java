2b63dcb1d64cde6cf2471238901438f83a6aa4f3
jaswoo <woodformed@gmail.com>
2019-06-19 11:54:58 -0700

    CheckPoint 1 - Changes to color, background image, buttons, altering activity_main.xml, colors.xml, MainActivity.java, strings.xml, zodiac_image.jpg 2019-06-19

--- app/src/main/java/jaswoo/quizapp/MainActivity.java
+++ app/src/main/java/jaswoo/quizapp/MainActivity.java
@@ -2,11 +2,15 @@ package jaswoo.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -21,7 +25,18 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFeedback = (TextView) findViewById(R.id.feedback_textview);
 
+        mTrueButton.setOnClickListener(this);
+        mFalseButton.setOnClickListener(this);
+        mFeedback.setOnClickListener(this);
 
+    }
 
+    @Override
+    public void onClick(View view) {
+        TextView myTextView = (TextView) view;
+        Toast myToast = Toast.makeText(this, "You just clicked " + myTextView.getText(), Toast.LENGTH_SHORT);
+        myToast.setGravity(Gravity.TOP, 0, 0);
+
+        myToast.show();
     }
 }
